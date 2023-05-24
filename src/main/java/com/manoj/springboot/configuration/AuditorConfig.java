600128a59ada7482750338dfc3b5420c57283a74
Manoj Basnet <basnetm02@gmail.com>
2023-05-24 17:32:28 +0545

    Jpa Auditor Configured for auditing book changes

---
+++ src/main/java/com/manoj/springboot/configuration/AuditorConfig.java
@@ -0,0 +1,26 @@
+package com.manoj.springboot.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditorConfig {
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return new AuditorAware<String>() {
+            @Override
+            public Optional<String> getCurrentAuditor() {
+                Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+                String username=authentication.getName();
+                return Optional.of(username);
+            }
+        };
+    }
+}
